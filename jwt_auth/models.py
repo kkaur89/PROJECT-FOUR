d4915c98dffcde12db137fac9762655020f17e0e
kkaur89 <karenjeetkaurp@gmail.com>
2021-04-14 15:59:49 +0100

    added auth and register

---
+++ jwt_auth/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from autoslug import AutoSlugField
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    email = models.CharField(max_length=50, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    profile_image = models.CharField(max_length=300)
+    slug = AutoSlugField(populate_from='username')
+    bio = models.CharField(max_length=255, blank=True)
+    friends = models.ManyToManyField('jwt_auth.User', related_name="jwt_auth", blank=True)
