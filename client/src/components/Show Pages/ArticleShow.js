95997bec9b247a3a240624913729f4964944c490
kkaur89 <karenjeetkaurp@gmail.com>
2021-04-17 18:46:25 +0100

    added linebreak to article text

--- client/src/components/Show Pages/ArticleShow.js
+++ client/src/components/Show Pages/ArticleShow.js
@@ -42,111 +42,112 @@ const ArticleShow = () => {
   // if (!article.comments.owner) return article.id
 
   return (
-
-    <div className="article-section">
-      <div className="article-hero-body">
-        <img className="article-logo-homepage" src='/assets/logo_white_large.png' />
-      </div>
-      <Jumbotron fluid className="article-jt-one">
-        <Container>
-          <h4>{article.name}</h4>
-          <h6>{article.author}</h6>    
-          <img
-            width={400}
-            height={300}
-            className="float-left mr-2 mb-1"
-            src={article.image[0]}
-            alt="Generic placeholder"
-          />
-          <p>
-            {article.text}
-          </p>
-        </Container>
-      </Jumbotron>
-      <Jumbotron fluid className="article-jt">
-        <Container> 
-          <img
-            width={400}
-            height={300}
-            className="float-right mr-2 mb-1"
-            src={article.image[1]}
-            alt="Generic placeholder"
-          />
-          <p>
-            {article.text_two}
-          </p>
-        </Container>
-      </Jumbotron>
-      <Jumbotron fluid className="article-jt">
-        <Container> 
-          <img
-            width={400}
-            height={300}
-            className="float-left mr-2 mb-1"
-            src={article.image[2]}
-            alt="Generic placeholder"
-          />
-          <p>
-            {article.text_three}
-          </p>
-        </Container>
-      </Jumbotron>
-      <Jumbotron fluid className="article-jt">
-        <Container> 
-          <img
-            width={400}
-            height={300}
-            className="float-right mr-2 mb-1"
-            src={article.image[3]}
-            alt="Generic placeholder"
-          />
-          <p>
-            {article.text_four}
-          </p>
-        </Container>
-      </Jumbotron>
-      <Jumbotron fluid className="article-jt">
-        <Container> 
-          <img
-            width={400}
-            height={300}
-            className="float-left mr-2 mb-1"
-            src={article.image[4]}
-            alt="Generic placeholder"
-          />
-          <p>
-            {article.text_five}
-          </p>
-        </Container>
-      </Jumbotron>
-      <Jumbotron fluid className="article-jt">
-        <Container> 
-          <img
-            width={400}
-            height={300}
-            className="float-right mr-2 mb-1"
-            src={article.image[5]}
-            alt="Generic placeholder"
-          />
-          <p>
-            {article.text_six}
-          </p>
-        </Container>
-      </Jumbotron>
-      <Jumbotron fluid className="article-jt">
-        <Container> 
-          <p>
+    <>
+      <div className="article-section">
+        <div className="article-hero-body">
+          <img className="article-logo-homepage" src='/assets/logo_white_large.png' />
+        </div>
+        <Jumbotron fluid className="article-jt-one">
+          <Container>
+            <h4>{article.name}</h4>
+            <h6>{article.author}</h6>    
+            <img
+              width={400}
+              height={300}
+              className="float-left mr-2 mb-1"
+              src={article.image[0]}
+              alt="Generic placeholder"
+            />
+            <p id="p_wrap">
+              {article.text}
+            </p>
+          </Container>
+        </Jumbotron>
+        <Jumbotron fluid className="article-jt">
+          <Container> 
+            <img
+              width={400}
+              height={300}
+              className="float-right mr-2 mb-1"
+              src={article.image[1]}
+              alt="Generic placeholder"
+            />
+            <p id="p_wrap">
+              {article.text_two}
+            </p>
+          </Container>
+        </Jumbotron>
+        <Jumbotron fluid className="article-jt">
+          <Container> 
+            <img
+              width={400}
+              height={300}
+              className="float-left mr-2 mb-1"
+              src={article.image[2]}
+              alt="Generic placeholder"
+            />
+            <p id="p_wrap">
+              {article.text_three}
+            </p>
+          </Container>
+        </Jumbotron>
+        <Jumbotron fluid className="article-jt">
+          <Container> 
+            <img
+              width={400}
+              height={300}
+              className="float-right mr-2 mb-1"
+              src={article.image[3]}
+              alt="Generic placeholder"
+            />
+            <p id="p_wrap">
+              {article.text_four}
+            </p>
+          </Container>
+        </Jumbotron>
+        <Jumbotron fluid className="article-jt">
+          <Container> 
+            <img
+              width={400}
+              height={300}
+              className="float-left mr-2 mb-1"
+              src={article.image[4]}
+              alt="Generic placeholder"
+            />
+            <p id="p_wrap">
+              {article.text_five}
+            </p>
+          </Container>
+        </Jumbotron>
+        <Jumbotron fluid className="article-jt">
+          <Container> 
+            <img
+              width={400}
+              height={300}
+              className="float-right mr-2 mb-1"
+              src={article.image[5]}
+              alt="Generic placeholder"
+            />
+            <p id="p_wrap">
+              {article.text_six}
+            </p>
+          </Container>
+        </Jumbotron>
+        <Jumbotron fluid className="article-jt">
+          <Container> 
+            <p id="p_wrap">
           Comments: 
-            <hr />
-            {article.comments[0].owner.username} - {article.comments[0].text}
-          </p>
-          <p>
-            <Button variant="primary" onClick={handleClick}>Like {article.like.length}</Button>
-          </p>
-        </Container>
-      </Jumbotron>
+              <hr />
+              {article.comments[0].owner.username} - {article.comments[0].text}
+            </p>
+            <p>
+              <Button variant="primary" onClick={handleClick}>Like {article.like.length}</Button>
+            </p>
+          </Container>
+        </Jumbotron>
  
-    </div>
+      </div>
+    </>
   )
 
 
