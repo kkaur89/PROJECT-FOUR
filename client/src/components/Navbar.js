96c8e83ef01cdc036978f2fb6d02458b0e4c8dea
kkaur89 <karenjeetkaurp@gmail.com>
2021-04-19 14:55:19 +0100

    added login modal

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,43 @@
-import React from 'react'
-//import axios from 'axios'
+import React, { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import axios from 'axios'
 
 // Boostrap
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
+import Modal from 'react-bootstrap/Modal'
+import Button from 'react-bootstrap/Button'
 
 
 const Navbar1 = () => {
+  
+  const [show, setShow] = useState(false)
+
+  const handleClose = () => setShow(false)
+  const handleShow = () => setShow(true)
+
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+  const history = useHistory()
+
+  const handleChange = event => {
+    const newFormData = { ...formData, [event.target.name]: event.target.value }
+    setFormData(newFormData)
+  }
+  console.log(formData)
+
+  const handleSubmit = async event => {
+    event.preventDefault()
+    const response = await axios.post('api/auth/login/', formData)
+    window.localStorage.setItem('token', response.data.token)
+    console.log(response.status)
+    history.push('/main')
+ 
+  }
+
+
   return (
     <>
       <Navbar expand="lg" className="bring-to-front" >
@@ -16,11 +47,51 @@ const Navbar1 = () => {
           <Nav className="mr-auto">
             <Nav.Link href="/main" className="nav-text">Home</Nav.Link>
             <Nav.Link href="/register"className="nav-text">Register</Nav.Link>
-            <Nav.Link href="#link"className="nav-text">Login</Nav.Link>
+            <Nav.Link href="#link"className="nav-text" onClick={handleShow}>Login</Nav.Link>
             <Nav.Link href="#link"className="nav-text">Profile</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Navbar>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+        </Modal.Header>
+        <Modal.Body>
+          <form className="column is-half is-offset-one-quarter box"
+            onSubmit={handleSubmit}>
+            <div className="field">
+              <label className="label">Email</label>
+              <div className="control">
+                <input
+                  className="input"
+                  placeholder="Email"
+                  name="email"
+                  value={formData.email}
+                  onChange={(event) => handleChange(event)}
+                />
+              </div>
+            </div>
+            <div className="field">
+              <label className="label">Password</label>
+              <div className="control">
+                <input
+                  className="input"
+                  type="password"
+                  placeholder="Password"
+                  name="password"
+                  value={formData.password}
+                  onChange={(event) => handleChange(event)}
+                />
+              </div>
+            </div>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" type="submit" onClick={handleClose}>
+            Login
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </>
   )
 }
--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -21,11 +21,12 @@ const Register = () => {
 
   const handleChange = (event) => {
     //? set state when user types
-    console.log('value', event.target.value)
+    // console.log('value', event.target.value)
     // console.log(formData['username'])
     const newFormData = { ...formData, [event.target.name]: event.target.value }
     setFormData(newFormData)
   }
+  console.log(formData)
 
   const handleSubmit = async event => {
     console.log('handleSubmit is working')
@@ -41,77 +42,83 @@ const Register = () => {
   // console.log(errors)
 
   return (
-    <div className="register">
-      <section className="section">
-        <div className="container">
-          <div className="columns">
-            <form className="column is-half is-offset-one-quarter box"
-              onSubmit={handleSubmit}
-            >
-              <div className="field">
-                <label className="label">Username</label>
-                <div className="control">
-                  <input
-                    // className={`input ${errors.username ? 'is-danger' : ''}`}
-                    placeholder="Username"
-                    name="username"
-                    value={formData.username}
-                    onChange={handleChange}
-                  />
-                </div>
-                {/* {errors.username && <p className="help is-danger">{errors.username}</p>} */}
-              </div>
-              <div className="field">
-                <label className="label">Email</label>
-                <div className="control">
-                  <input
-                    // className={`input ${errors.email ? 'is-danger' : ''}`}
-                    placeholder="Email"
-                    name="email"
-                    value={formData.email}
-                    onChange={handleChange}
-                  />
-                </div>
-                {/* {errors.email && <p className="help is-danger">{errors.email}</p>} */}
-              </div>
-              <div className="field">
-                <label className="label">Password</label>
-                <div className="control">
-                  <input
-                    className={`input ${errors.password ? 'is-danger' : ''}`}
-                    type="password"
-                    placeholder="Password"
-                    name="password"
-                    value={formData.password}
-                    onChange={handleChange}
-                  />
-                </div>
-                {errors.password && <p className="help is-danger">{errors.password}</p>}
-              </div>
-              <div className="field">
-                <label className="label">Password Confirmation</label>
-                <div className="control">
-                  <input
-                    className={`input ${errors.passwordConfirmation ? 'is-danger' : ''}`}
-                    type="password"
-                    placeholder="Password Confirmation"
-                    name="passwordConfirmation"
-                    value={formData.passwordConfirmation}
-                    onChange={handleChange}
-                  />
-                </div>
-                {errors.passwordConfirmation && <p className="help is-danger">{errors.passwordConfirmation}</p>}
-              </div>
-              <div className="field">
+    <>
+      <div className="register-hero-body">
+        <img className="register-logo-homepage" src='/assets/logo_white_large.png' />
+   
+        <div className="register">
+          <section className="register-section">
+            <div className="container">
+              <div className="columns">
+                <form className="column is-half is-offset-one-quarter box"
+                  onSubmit={handleSubmit}
+                >
+                  <div className="field">
+                    <label className="label">Username</label>
+                    <div className="control">
+                      <input
+                        className={`input ${errors.username ? 'is-danger' : ''}`}
+                        placeholder="Username"
+                        name="username"
+                        value={formData.username}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    {errors.username && <p className="help is-danger">{errors.username}</p>}
+                  </div>
+                  <div className="field">
+                    <label className="label">Email</label>
+                    <div className="control">
+                      <input
+                        className={`input ${errors.email ? 'is-danger' : ''}`}
+                        placeholder="Email"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    {errors.email && <p className="help is-danger">{errors.email}</p>}
+                  </div>
+                  <div className="field">
+                    <label className="label">Password</label>
+                    <div className="control">
+                      <input
+                        className={`input ${errors.password ? 'is-danger' : ''}`}
+                        type="password"
+                        placeholder="Password"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    {errors.password && <p className="help is-danger">{errors.password}</p>}
+                  </div>
+                  <div className="field">
+                    <label className="label">Password Confirmation</label>
+                    <div className="control">
+                      <input
+                        className={`input ${errors.passwordConfirmation ? 'is-danger' : ''}`}
+                        type="password"
+                        placeholder="Password Confirmation"
+                        name="passwordConfirmation"
+                        value={formData.passwordConfirmation}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    {errors.passwordConfirmation && <p className="help is-danger">{errors.passwordConfirmation}</p>}
+                  </div>
+                  <div className="field">
 
-                <button type="submit" className="button register-button">Click to Register</button>
+                    <button type="submit" className="button register-button">Click to Register</button>
 
+                  </div>
+                </form>
               </div>
-            </form>
-          </div>
-        </div>
-      </section >
-    </div >
+            </div>
+          </section >
+        </div >
+      </div>
+    </>
   )
 }
 
